bc0d6541d2f30387a28c04121cfa470045cc84b4
gyn <2394004890@qq.com>
2026-04-16 19:24:31 +0800

    定义数据库

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from web.models.user import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ("user",) #逗号不能删，因为要是列表，这样管理员那里点击搜索会分页。不加会是下拉框，用户太多会卡死。
 
 # Register your models here.
