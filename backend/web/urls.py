fbf7c115f00c5d243ea1899c1c6950e709aaa3ac
gyn <2394004890@qq.com>
2026-04-17 16:16:19 +0800

    前端对接注册、登录、退出api

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.views import LogoutView
 from django.urls import path
 from web.views.index import index
 from web.views.user.account.login import LoginView
+from web.views.user.account.logout import LogoutView
 from web.views.user.account.refresh_token import RefreshTokenView
 from web.views.user.account.register import RegisterView
 
--- backend/web/views/user/account/logout.py
+++ backend/web/views/user/account/logout.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import  Response
 from rest_framework.permissions import IsAuthenticated
 
-class LogoutAPIView(APIView):
+class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
     def post(self, request):
         response = Response({
