c6fb0291a8bd630ae2786defc40fd940ac1eb82b
gyn <2394004890@qq.com>
2026-04-23 16:34:06 +0800

    实现好友页面后端

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
+
+from web.models.friend import Friend
 from web.models.user import UserProfile
 from web.models.character import Character
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    raw_id_fields = ("user",) #逗号不能删，因为要是列表，这样管理员那里点击搜索会分页。不加会是下拉框，用户太多会卡死。
+    raw_id_fields = ('user',) #逗号不能删，因为要是列表，这样管理员那里点击搜索会分页。不加会是下拉框，用户太多会卡死。
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
-    raw_id_fields = ("author",)
+    raw_id_fields = ('author',)
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    raw_id_fields = ('me', 'character',)
+
+
 # Register your models here.
