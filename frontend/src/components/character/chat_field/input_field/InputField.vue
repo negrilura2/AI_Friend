27b1ef1bd5f82f31c98b73559b3748c7c34f7a72
gyn <2394004890@qq.com>
2026-04-26 19:16:50 +0800

    实现聊天记录的流式加载前端+get_history代码

--- frontend/src/components/character/chat_field/input_field/InputField.vue
+++ frontend/src/components/character/chat_field/input_field/InputField.vue
@@ -6,6 +6,7 @@ import {ref, useTemplateRef} from "vue";
 import streamApi from "@/js/http/streamApi.js";
 
 const props = defineProps(['friendId'])
+const emit = defineEmits(['pushBackMessage','addToLastMessage'])
 const inputRef = useTemplateRef('input-ref')
 const message = ref('')
 let isProcessing = false
@@ -20,6 +21,10 @@ async function handleSend(){
   if(!content) return
   message.value = ''
 
+
+  emit('pushBackMessage',{role: 'user', content: content, id: crypto.randomUUID()})
+  emit('pushBackMessage',{role: 'ai', content: '', id: crypto.randomUUID()})
+
   try {
     await streamApi('/api/friend/message/chat/', {
       body: {
@@ -30,7 +35,7 @@ async function handleSend(){
         if (isDone) {
           isProcessing = false
         } else if (data.content) {
-          console.log(data.content)
+          emit('addToLastMessage', data.content)
         }
       },
       onerror(err) {
@@ -38,7 +43,6 @@ async function handleSend(){
       },
     })
   } catch (err) {
-    console.log(err)
     isProcessing = false
   }
 }
