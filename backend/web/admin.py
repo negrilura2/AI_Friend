b48813d1217fc77457f9eaf1281938c21e89cb05
gyn <2394004890@qq.com>
2026-05-06 14:27:22 +0800

    实现音色的自由选择

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from web.models.friend import Friend,Message,SystemPrompt
 from web.models.user import UserProfile
-from web.models.character import Character
+from web.models.character import Character, Voice
 from web.views.friend.message.chat.graph import ChatGraph
 
 
@@ -12,7 +12,9 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
-    raw_id_fields = ('author',)
+    raw_id_fields = ('author', 'voice')
+
+admin.site.register(Voice)
 
 @admin.register(Friend)
 class FriendAdmin(admin.ModelAdmin):
